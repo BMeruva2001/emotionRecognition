c7306fef655aebd37969a367a7d9c4d6e033bf05
BMeruva2001 <67717516+BMeruva2001@users.noreply.github.com>
2021-05-28 22:59:54 +0530

    initial version

---
+++ main.js
@@ -0,0 +1,32 @@
+const video = document.getElementById('video')
+const MODEL_URL = process.env.PUBLIC_URL + "/models"
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromDisk(MODEL_URL),
+  faceapi.nets.faceLandmark68Net.loadFromDisk(MODEL_URL),
+  faceapi.nets.faceRecognitionNet.loadFromDisk(MODEL_URL),
+  faceapi.nets.faceExpressionNet.loadFromDisk(MODEL_URL)
+]).then(startVideo)
+
+function startVideo() {
+    navigator.mediaDevices.getUserMedia(
+        { video: {} },
+        stream => video.srcObject = stream,
+        err => console.error(err)
+    )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
+})
